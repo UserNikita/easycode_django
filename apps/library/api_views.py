ce907e72e39baab9fe6ba3a0ce3ed538d6371454
nikita123 <nikita-e-mail@mail.ru>
2018-09-08 16:52:36 +0400

    Небольшой рефакторинг

--- apps/library/api_views.py
+++ apps/library/api_views.py
@@ -1,8 +1,13 @@
-from rest_framework import viewsets
-from .models import Category
-from .serializers import CategorySerializer
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from .models import Category, Book
+from .serializers import CategorySerializer, BookSerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class BookViewSet(ReadOnlyModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
