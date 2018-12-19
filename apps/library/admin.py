c8040bcbf9a31a34529d4a762d5aaf95c303357f
nikita123 <nikita-e-mail@mail.ru>
2018-12-19 23:15:34 +0400

    Добавлен фильтр в админке позволяющий фильтровать книги по наличию миниатюры для обложки

--- apps/library/admin.py
+++ apps/library/admin.py
@@ -3,6 +3,24 @@ from .models import *
 from .forms import BookAdminForm
 
 
+class CoverThumbnailFilter(admin.SimpleListFilter):
+    """
+    Класс для фильтрации книг в админке по наличию миниатюры для обложки
+    """
+    title = "Миниатюрная обложка"
+    parameter_name = 'cover_thumbnail'
+
+    def lookups(self, request, model_admin):
+        return ('no', "Нет"), ('yes', "Есть")
+
+    def queryset(self, request, queryset):
+        if self.value() == 'no':
+            return queryset.filter(cover_thumbnail='')
+        elif self.value() == 'yes':
+            return queryset.exclude(cover_thumbnail='')
+        return queryset
+
+
 class BookFileInline(admin.StackedInline):
     model = BookFile
     extra = 0
@@ -10,7 +28,7 @@ class BookFileInline(admin.StackedInline):
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('get_cover', 'title', 'year',)
-    list_filter = ('year', 'category', 'publishers', 'authors',)
+    list_filter = ('year', 'category', CoverThumbnailFilter, 'publishers', 'authors',)
     filter_horizontal = ('authors', 'publishers', 'tags',)
     inlines = (BookFileInline,)
     form = BookAdminForm
