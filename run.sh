d8266a37e76bfc9568c3ed66d601285c8e9f7efe
usernikita <nikita-e-mail@mail.ru>
2020-05-22 16:18:50 +0300

    Исправлен run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 sleep 5
-python manage.py migrate &
-python manage.py collectstatic --no-input &
+python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py loaddata library_data.json
+python manage.py loaddata quiz_data.json
 python manage.py runserver 0.0.0.0:8000
 #gunicorn wsgi:application --bind 0.0.0.0:8000
