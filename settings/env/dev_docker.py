cb5006b599def4eeaa8be483a2aef1f84a7932cd
usernikita <nikita-e-mail@mail.ru>
2020-06-05 18:36:01 +0300

    Добавлен django-debug-toolbar

--- settings/env/dev_docker.py
+++ settings/env/dev_docker.py
@@ -1,3 +1,4 @@
+import socket
 from settings.base import *
 
 
@@ -5,8 +6,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+ip = socket.gethostbyname(socket.gethostname())
+INTERNAL_IPS = [
+    '127.0.0.1',
+    ip[:-1] + '1',  # tricks to have debug toolbar when developing with docker
+]
+
+
 INSTALLED_APPS += [
     'django_extensions',
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 DATABASES = {
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -12,3 +13,9 @@ urlpatterns = [
     url(r'^summernote/', include('django_summernote.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
