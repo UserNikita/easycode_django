dd5ee3fcd060bff42d94f344d5882e2df9d9cb01
usernikita <nikita-e-mail@mail.ru>
2024-11-10 07:30:32 +0400

    Добавлена связка имеющегося в бд видео с плейлистом при получении видео из плейлиста

--- apps/youtube/admin.py
+++ apps/youtube/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Channel, Playlist, Video, VideoView
 from .utils.parse_playlists import parse_playlists
-from .utils.parse_videos import parse_videos
+from .utils.parse_videos import parse_videos, parse_videos_in_playlist
 
 
 @admin.register(Channel)
@@ -25,11 +25,18 @@ class ChannelAdmin(admin.ModelAdmin):
 @admin.register(Playlist)
 class PlaylistAdmin(admin.ModelAdmin):
     list_display = ["title", "url", "channel"]
+    actions = ["parse_videos"]
+
+    def parse_videos(self, request, queryset):
+        for playlist in queryset:
+            parse_videos_in_playlist(playlist=playlist)
+
+    parse_videos.short_description = "Получить видеоролики"
 
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ["title", "url", "publish_date"]
+    list_display = ["title", "publish_date", "channel", "playlist"]
 
 
 @admin.register(VideoView)
--- apps/youtube/utils/parse_videos.py
+++ apps/youtube/utils/parse_videos.py
@@ -1,13 +1,13 @@
-from pytubefix import Channel
+from pytubefix import Channel, Playlist
 
 from apps.youtube import models
 
 
 def parse_videos(channel: models.Channel):
-    yt = Channel(channel.url)
-
-    for v in yt.videos:
-        video = models.Video(
+    for v in Channel(channel.url).videos:
+        if models.Video.objects.filter(pk=v.video_id).exists():
+            continue
+        models.Video.objects.create(
             id=v.video_id,
             url=v.watch_url,
             title=v.title,
@@ -16,9 +16,23 @@ def parse_videos(channel: models.Channel):
             thumbnail_url=v.thumbnail_url,
             publish_date=v.publish_date,
             channel=channel,
-            # playlist =
         )
-        try:
+
+
+def parse_videos_in_playlist(playlist: models.Playlist):
+    for v in Playlist(url=playlist.url).videos:
+        if video := models.Video.objects.filter(id=v.video_id).first():
+            video.playlist = playlist
             video.save()
-        except:
-            pass
+        else:
+            models.Video.objects.create(
+                id=v.video_id,
+                url=v.watch_url,
+                title=v.title,
+                description=v.description.encode("utf-8"),  # TODO: Поправить баг с юникодом
+                duration=v.length,
+                thumbnail_url=v.thumbnail_url,
+                publish_date=v.publish_date,
+                channel=playlist.channel,
+                playlist=playlist,
+            )
