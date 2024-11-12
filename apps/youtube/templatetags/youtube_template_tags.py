f3f5bb902676a1931329c4388926c8361497103d
usernikita <nikita-e-mail@mail.ru>
2024-11-13 01:50:19 +0400

    Добавлены пункты меню в сайдбар со списком плейлистов

--- apps/youtube/templatetags/youtube_template_tags.py
+++ apps/youtube/templatetags/youtube_template_tags.py
@@ -6,7 +6,10 @@ register = template.Library()
 
 @register.inclusion_tag(filename="youtube/playlist_sidebar.html", takes_context=True)
 def include_playlists(context):
+    channel = context["channel"]
     context.update({
-        'playlists': context["channel"].playlist_set.all().annotate(videos_count=Count("video")),
+        'playlists': channel.playlist_set.all().annotate(videos_count=Count("video")),
+        'without_playlist_videos_count': channel.video_set.filter(playlist=None).count(),
+        'all_videos_count': channel.video_set.count(),
     })
     return context
